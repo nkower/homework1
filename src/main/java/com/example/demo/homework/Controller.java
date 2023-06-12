36f5ca83d6d4d1f01e51c9042990b426bc51bca6
nkower <sbgofspeed@naver.com>
2023-06-12 15:48:43 +0900

    dev

--- src/main/java/com/example/demo/homework/Controller.java
+++ src/main/java/com/example/demo/homework/Controller.java
@@ -1,2 +1,13 @@
-package com.example.demo.homework;public class Controller {
+package com.example.demo.homework;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String getmessage(){
+        return "To-do Application !";
+    }
 }
